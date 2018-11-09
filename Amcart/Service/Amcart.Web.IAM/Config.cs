2dff0a6759ac92ca9863ceca76b0fb288f258ac8
Rahul Bansal <rahul.bansal@nagarro.com>
2018-11-09 14:01:14 +0530

    Adding IAM service

---
+++ Amcart/Service/Amcart.Web.IAM/Config.cs
@@ -0,0 +1,61 @@
+﻿using IdentityServer4;
+using IdentityServer4.Models;
+using IdentityServer4.Test;
+using System.Collections.Generic;
+
+namespace Amcart.Web.IAM
+{
+    public class Config
+    {
+        // clients want to access resources (aka scopes)
+        public static IEnumerable<Client> GetClients()
+        {
+            // client credentials client
+            return new List<Client>
+            {
+                new Client
+                {
+                    ClientId = "client",
+                    ClientName = "MVC Client",
+                    ClientSecrets = new List<Secret>()
+                    {
+                        new Secret("secret".Sha256())
+                    },
+
+                    AllowedGrantTypes = GrantTypes.ResourceOwnerPasswordAndClientCredentials,
+                    RequireConsent = false,
+                    
+                    AllowedScopes = new List<string>
+                    {
+                        IdentityServerConstants.StandardScopes.OpenId,
+                        IdentityServerConstants.StandardScopes.Profile
+                    },
+
+                    AllowAccessTokensViaBrowser = true
+                }
+            };
+        }
+
+        public static List<IdentityResource> GetIdentityResources()
+        {
+            return new List<IdentityResource>
+            {
+                new IdentityResources.OpenId(),
+                new IdentityResources.Profile() // <-- usefull
+            };
+        }
+
+        public static List<TestUser> GetUsers()
+        {
+            return new List<TestUser>
+            {
+                new TestUser
+                {
+                    SubjectId = "1",
+                    Username = "rahul",
+                    Password = "Passw0rd"
+                }
+            };
+        }
+    }
+}
