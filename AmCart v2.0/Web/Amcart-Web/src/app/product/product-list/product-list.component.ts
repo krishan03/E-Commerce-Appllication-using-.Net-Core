547b6be83f33509f7464561b8a6915e8f4b2a1b5
kashish kumar <kumarka@dss.nyc.gov>
2018-12-18 17:41:52 +0530

    Product Listing

--- AmCart v2.0/Web/Amcart-Web/src/app/product/product-list/product-list.component.ts
+++ AmCart v2.0/Web/Amcart-Web/src/app/product/product-list/product-list.component.ts
@@ -1,24 +1,34 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, OnDestroy } from '@angular/core';
 import { Product } from 'src/app/models/product-item';
 import { ProductService } from 'src/app/core/services/product.service';
+import { Subject } from 'rxjs';
+import { takeUntil } from 'rxjs/operators';
 
 @Component({
   selector: 'app-product-list',
   templateUrl: './product-list.component.html',
   styleUrls: ['./product-list.component.css']
 })
-export class ProductListComponent implements OnInit {
+export class ProductListComponent implements OnInit, OnDestroy {
 
   @Input() products: Array<Product>;
+  destroy$: Subject<boolean> = new Subject<boolean>();
 
   constructor(private _productService: ProductService) { }
 
   ngOnInit() {
     this.products = [];
-    this._productService.getNewArrivedProducts().subscribe((productList: Array<any>) => {
-      if (productList)
-        this.products = productList['Data'];
-    });
+    this._productService.getNewArrivedProducts()
+      .pipe(takeUntil(this.destroy$))
+      .subscribe((productList: Array<any>) => {
+        if (productList)
+          this.products = productList['Data'];
+      });
+  }
+
+  ngOnDestroy(): void {
+    this.destroy$.next(true);
+    this.destroy$.unsubscribe();
   }
 
 }
