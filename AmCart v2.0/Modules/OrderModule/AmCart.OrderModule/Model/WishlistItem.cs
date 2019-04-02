d83068fe50f8db64e6f2fa9467424a0e6615f7ae
Rahul Bansal <rahul.bansal@nagarro.com>
2019-04-02 19:41:49 +0530

    Created domain for Cart and wishlist

---
+++ AmCart v2.0/Modules/OrderModule/AmCart.OrderModule/Model/WishlistItem.cs
@@ -0,0 +1,13 @@
+﻿using AmCart.Core.Domain;
+using MongoDB.Bson;
+using System.Collections.Generic;
+
+namespace AmCart.OrderModule.Model
+{
+    public class Wishlist : DomainBase
+    {
+        public ObjectId UserId { get; set; }
+
+        public IList<ObjectId> Products { get; set; }
+    }
+}
